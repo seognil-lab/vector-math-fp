f2e9ad4a654660d145a136f51fedf199952804d3
seognil LC <seognil@gmail.com>
2019-06-27 14:38:13 +0800

    feat: update source code to fit latest Airbnb eslint

--- src/index.js
+++ src/index.js
@@ -15,104 +15,104 @@ import {
 import approxFix from 'approx-fix';
 
 // e.g.:
-// _math(divide)(v1)(v2) => v2 / 1v
-// _rotate(angle)(v) => v ~
-// _math(divide)([1, 2])([4, 6]) => [4, 3]
+// Vmath(divide)(v1)(v2) => v2 / 1v
+// Vrotate(angle)(v) => v ~
+// Vmath(divide)([1, 2])([4, 6]) => [4, 3]
 
 // * -------------------------------- tools
 
 // * support Array and Object
-const _math = curry((fn, v2, v1) =>
+const Vmath = curry((fn, v2, v1) =>
     Array.isArray(v1) ? zipWith(fn, v1, v2) : mergeWith(fn, cloneDeep(v1), v2),
 );
 
-const _arrToObj = curry((keys, arr) => keys.reduce((o, k, i) => ((o[k] = arr[i] || 0), o), {}));
-const _objToArr = curry((keys, obj) => keys.map(k => obj[k] || 0));
-const _radianToDegree = n => (n * 180) / Math.PI;
-const _degreeToRadian = n => (n / 180) * Math.PI;
-const _formatAngle = angle =>
-    Math.abs(angle % 360) == 180 ? 180 : (((angle % 360) + 540) % 360) - 180;
+const VarrToObj = curry((keys, arr) =>
+    keys.reduce((o, k, i) => Object.assign(o, { [k]: arr[i] || 0 }), {}),
+);
+const VobjToArr = curry((keys, obj) => keys.map(k => obj[k] || 0));
+const VradianToDegree = n => (n * 180) / Math.PI;
+const VdegreeToRadian = n => (n / 180) * Math.PI;
+const VformatAngle = angle =>
+    Math.abs(angle % 360) === 180 ? 180 : (((angle % 360) + 540) % 360) - 180;
 
 // * ---------------- simple math
 
-const _minus = _math(subtract);
-const _add = _math(add);
-const _times = _math(multiply);
-const _divide = _math(divide);
-const _dot = curry((v1, v2) => v1[0] * v2[0] + v1[1] * v2[1]); // == * cos(tha)
-const _cross = curry((v1, v2) => v1[0] * v2[1] - v2[0] * v1[1]); // == * sin(tha)
+const Vminus = Vmath(subtract);
+const Vadd = Vmath(add);
+const Vtimes = Vmath(multiply);
+const Vdivide = Vmath(divide);
+const Vdot = curry((v1, v2) => v1[0] * v2[0] + v1[1] * v2[1]); // === * cos(tha)
+const Vcross = curry((v1, v2) => v1[0] * v2[1] - v2[0] * v1[1]); // === * sin(tha)
 
 // * ---------------- vector property
 
-const _lenOf = pipe([map(e => Math.pow(e, 2)), sum, Math.sqrt]);
-const _areaOf = v => multiply(...v);
-const _ratioOf = ([x, y]) => (!y ? NaN : x / y); // 16:9 => 16 / 9
+const VlenOf = pipe([map(e => e ** 2), sum, Math.sqrt]);
+const VareaOf = v => multiply(...v);
+const VratioOf = ([x, y]) => (!y ? NaN : x / y); // 16:9 => 16 / 9
 
 // * -------------------------------- rotate and angle
 
-const _angleBetween = curry((v1, v2) => {
-    let s = _lenOf(v1) * _lenOf(v2);
-    if (s == 0) return 0;
+const VangleBetween = curry((v1, v2) => {
+    const s = VlenOf(v1) * VlenOf(v2);
+    if (s === 0) return 0;
 
-    let r = _dot(v1, v2) / s;
+    let r = Vdot(v1, v2) / s;
     r = Math.min(r, 1);
-    return approxFix(_radianToDegree(Math.acos(r) * (_cross(v1, v2) < 0 ? -1 : 1)));
+    return approxFix(VradianToDegree(Math.acos(r) * (Vcross(v1, v2) < 0 ? -1 : 1)));
 });
 
-const _angleOf = _angleBetween([1, 0]);
+const VangleOf = VangleBetween([1, 0]);
 
-const _vectorCase = {
+const VvectorCase = {
     '-90': [0, -1],
     0: [1, 0],
     90: [0, 1],
     180: [-1, 0],
 };
 
-const _vectorOf = angle => {
-    angle = _formatAngle(angle);
-    let radian;
-    return (
-        _vectorCase[angle] ||
-        ((radian = _degreeToRadian(angle)), [Math.cos(radian), Math.sin(radian)])
-    );
+const VvectorOf = angle => {
+    const normalAngle = VformatAngle(angle);
+    if (VvectorCase[normalAngle]) return VvectorCase[normalAngle];
+
+    const radian = VdegreeToRadian(normalAngle);
+    return [Math.cos(radian), Math.sin(radian)];
 };
 
-const _rotateRaw = (angle, v) => {
-    angle = angle || 0;
-    let c = _lenOf(v);
-    if (c == 0) return v;
+const VrotateRaw = (angle, v) => {
+    const c = VlenOf(v);
+    if (c === 0) return v;
 
-    const radian = _degreeToRadian(angle);
-    let cos1 = v[0] / c,
-        sin1 = v[1] / c,
-        cos2 = Math.cos(radian),
-        sin2 = Math.sin(radian);
+    const radian = VdegreeToRadian(angle);
+    const cos1 = v[0] / c;
+    const sin1 = v[1] / c;
+    const cos2 = Math.cos(radian);
+    const sin2 = Math.sin(radian);
     return [(cos1 * cos2 - sin1 * sin2) * c, (sin1 * cos2 + cos1 * sin2) * c].map(approxFix);
 };
-const _rotate = curry(_rotateRaw);
-const _unRotate = curry((angle, v) => _rotateRaw(-angle, v));
+const Vrotate = curry(VrotateRaw);
+const VunRotate = curry((angle, v) => VrotateRaw(-angle, v));
 
 // * -------------------------------- export
 
 export {
-    _math as math,
-    _arrToObj as arrToObj,
-    _objToArr as objToArr,
-    _radianToDegree as radianToDegree,
-    _degreeToRadian as degreeToRadian,
-    _formatAngle as formatAngle,
-    _minus as minus,
-    _add as add,
-    _times as times,
-    _divide as divide,
-    _dot as dot,
-    _cross as cross,
-    _lenOf as lenOf,
-    _areaOf as areaOf,
-    _ratioOf as ratioOf,
-    _angleBetween as angleBetween,
-    _angleOf as angleOf,
-    _vectorOf as vectorOf,
-    _rotate as rotate,
-    _unRotate as unRotate,
+    Vmath as math,
+    VarrToObj as arrToObj,
+    VobjToArr as objToArr,
+    VradianToDegree as radianToDegree,
+    VdegreeToRadian as degreeToRadian,
+    VformatAngle as formatAngle,
+    Vminus as minus,
+    Vadd as add,
+    Vtimes as times,
+    Vdivide as divide,
+    Vdot as dot,
+    Vcross as cross,
+    VlenOf as lenOf,
+    VareaOf as areaOf,
+    VratioOf as ratioOf,
+    VangleBetween as angleBetween,
+    VangleOf as angleOf,
+    VvectorOf as vectorOf,
+    Vrotate as rotate,
+    VunRotate as unRotate,
 };
